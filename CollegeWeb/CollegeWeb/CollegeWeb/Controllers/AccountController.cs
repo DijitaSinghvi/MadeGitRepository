c9d63b5ae4a8fa4053d8b1ecbd4eb2f7ae4bb94f
DijitaSinghvi <42962070+DijitaSinghvi@users.noreply.github.com>
2018-10-01 18:48:10 +0530

    code for drop down binding .

---
+++ CollegeWeb/CollegeWeb/CollegeWeb/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CollegeWeb.Models;
+
+namespace CollegeWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        CollegeContext db = new CollegeContext();
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            List<Course> courseList = new List<Course>();
+            List<Role> roleList = new List<Role>();
+            List<SelectListItem> courseListNew = new List<SelectListItem>();
+            List<SelectListItem> roleListNew = new List<SelectListItem>();
+            courseListNew = courseList.Select(x => new SelectListItem
+            {
+                Text = x.CourseName,
+                Value = x.CourseId.ToString()
+            }).ToList();
+            roleListNew = roleList.Select(x => new SelectListItem
+            {
+                Text = x.RoleName,
+                Value = x.RoleId.ToString()
+
+            }).ToList();
+            ViewBag.CourseShow = courseListNew;
+            ViewBag.RoleShow = roleListNew;
+
+
+            return View();
+        }
+    }
+}
